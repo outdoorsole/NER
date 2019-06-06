91596c82081706250575ae0685fa69534c6be969
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-06-05 19:34:51 -0700

    Initial Commit

---
+++ NER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NER
+//
+//  Created by Maribel Montejano on 6/5/19.
+//  Copyright © 2019 Maribel Montejano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
